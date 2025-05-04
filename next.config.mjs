e1fcb61f6f01c54a979dee60c4997befd62faf1d
gitrands <88889533+gitrands@users.noreply.github.com>
2025-05-04 10:43:11 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  ouput:"export",
+};
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
